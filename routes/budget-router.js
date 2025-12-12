1797782dc17c8dad5c14f3f9b7be848643c0533a
julien-p-code <229524075+julien-p-code@users.noreply.github.com>
2025-12-12 17:41:55 +0100

    Complete envelopes CRUD with transfer between envelopes endpoint

--- routes/budget-router.js
+++ routes/budget-router.js
@@ -3,6 +3,7 @@ const budgetRouter = express.Router();
 const budgetControllers = require('../controllers/budget-controller');
 
 const {
+    transferBetweenEnvelopes,
     getEnvelopes,
     getAvailableBudget,
     getTotalBudget,
@@ -13,7 +14,7 @@ const {
 } = budgetControllers;
 
 // Route to initialize total budget.
-budgetRouter.post('/init', (req, res, next) => {
+budgetRouter.post('/init', (req, res) => {
     const { totalBudget: initBudget } = req.body;
     const totalBudgetNum = Number(initBudget);
 
@@ -26,7 +27,7 @@ budgetRouter.post('/init', (req, res, next) => {
 
     setTotalBudget(totalBudgetNum);
 
-    res.status(200).json({
+    return res.status(200).json({
         success: true,
         message: 'Budget initialized successfully',
         totalBudget: getTotalBudget(),
@@ -35,7 +36,7 @@ budgetRouter.post('/init', (req, res, next) => {
 });
 
 // Route to create a new envelope.
-budgetRouter.post('/envelopes', (req, res, next) => {
+budgetRouter.post('/envelopes', (req, res) => {
     const { name, allocatedAmount } = req.body;
     const allocatedAmountNum = Number(allocatedAmount);
 
@@ -49,7 +50,7 @@ budgetRouter.post('/envelopes', (req, res, next) => {
 
     const newEnvelope = createEnvelope(name.trim(), allocatedAmountNum);
 
-    res.status(201).json({
+    return res.status(201).json({
         success: true,
         message: 'Envelope created successfully',
         envelope: newEnvelope,
@@ -57,8 +58,47 @@ budgetRouter.post('/envelopes', (req, res, next) => {
     });
 });
 
+// Route to transfer budget between envelopes.
+budgetRouter.post('/envelopes/transfer', (req, res) => {
+    const { fromEnvelopeId, toEnvelopeId, amount } = req.body;
+    const fromIdNum = Number(fromEnvelopeId);
+    const toIdNum = Number(toEnvelopeId);
+    const amountNum = Number(amount);
+
+    if (!Number.isFinite(fromIdNum) || fromIdNum < 0) {
+        return res.status(400).json({
+            success: false,
+            message: 'Invalid source envelope id'
+        });
+    };
+
+    if (!Number.isFinite(toIdNum) || toIdNum < 0) {
+        return res.status(400).json({
+            success: false,
+            message: 'Invalid target envelope id'
+        });
+    };
+    
+    if (!Number.isFinite(amountNum) || amountNum <= 0) {
+        return res.status(400).json({
+            success: false,
+            message: 'Invalid transfer amount'
+        });
+    }
+
+    const { fromEnvelope, toEnvelope } = transferBetweenEnvelopes(fromIdNum, toIdNum, amountNum);
+
+    return res.status(200).json({
+        success: true,
+        message: 'Transfer completed successfully',
+        fromEnvelope,
+        toEnvelope,
+        availableBudget: getAvailableBudget(),
+    });
+});
+
 // Route to modify an existing envelope.
-budgetRouter.put('/envelopes/:id', (req, res, next) => {
+budgetRouter.put('/envelopes/:id', (req, res) => {
     const id = Number(req.params.id);
     const { name, allocatedAmount } = req.body;
     const allocatedAmountNum = Number(allocatedAmount);
@@ -80,7 +120,7 @@ budgetRouter.put('/envelopes/:id', (req, res, next) => {
 
     const updatedEnvelope = modifyEnvelope(id, name.trim(), allocatedAmountNum);
 
-    res.status(200).json({
+    return res.status(200).json({
         success: true,
         message: 'Envelope updated successfully',
         envelope: updatedEnvelope,
@@ -88,7 +128,7 @@ budgetRouter.put('/envelopes/:id', (req, res, next) => {
     });
 });
 
-budgetRouter.delete('/envelopes/:id', (req, res, next) => {
+budgetRouter.delete('/envelopes/:id', (req, res) => {
     const id = Number(req.params.id);
 
     if (!Number.isFinite(id) || id < 0) {
@@ -100,27 +140,27 @@ budgetRouter.delete('/envelopes/:id', (req, res, next) => {
 
     deleteEnvelope(id);
 
-    res.status(204).send();
+    return res.status(204).send();
 });
 
 // Route to get all envelopes.
-budgetRouter.get('/status', (req, res, next) => {
-    res.status(200).json({
+budgetRouter.get('/status', (req, res) => {
+    return res.status(200).json({
         envelopes: getEnvelopes(),
         availableBudget: getAvailableBudget(),
         totalBudget: getTotalBudget(),
     });
 });
 
-budgetRouter.get('/envelopes', (req, res, next) => {
-    res.status(200).json({
+budgetRouter.get('/envelopes', (req, res) => {
+    return res.status(200).json({
         success: true,
         envelopes: getEnvelopes(),
     });
 });
 
 // Route to get an envelope by ID.
-budgetRouter.get('/envelopes/:id', (req, res, next) => {
+budgetRouter.get('/envelopes/:id', (req, res) => {
     const id = Number(req.params.id);
 
     if (!Number.isFinite(id) || id < 0) {
@@ -139,7 +179,7 @@ budgetRouter.get('/envelopes/:id', (req, res, next) => {
         });
     }
 
-    res.status(200).json({
+    return res.status(200).json({
         success: true,
         envelope
     });
